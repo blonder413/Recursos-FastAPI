9f2627d51b100b61426359d04551d4331e65af51
Jonathan Morales <blonder413@gmail.com>
2023-06-01 20:25:25 -0500

    servir archivos estaticos

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from routers import user
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 # user.router hace referencia al archivo user y a la instancia router
 app.include_router(user.router)
+
+# exponer recursos estáticos
+app.mount("/estatico", StaticFiles(directory="static"), name="ruta_static")
+# app.mount("/estatico", StaticFiles(directory="static/images"), name="ruta_static")
 
 @app.get("/")
 def inicio():
